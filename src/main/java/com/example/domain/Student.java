3375087087dd7af1fe2368c169fe5ac872007231
yangfan <yangfan@zcgit.com>
2017-02-10 14:48:26 +0800

    修改 update和delete方法的 method 请求方法类型

--- src/main/java/com/example/domain/Student.java
+++ src/main/java/com/example/domain/Student.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.EqualsAndHashCode;
@@ -19,7 +20,7 @@ public class Student implements Serializable {
 	private static final long serialVersionUID = 659925410003167992L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	private String name;
--- src/main/java/com/example/domain/StudentController.java
+++ src/main/java/com/example/domain/StudentController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,38 +14,30 @@ public class StudentController {
 	@Autowired
 	private StudentRepository studentRepository;
 
-	@RequestMapping("/list")
-	@ResponseBody
+	@RequestMapping(value = "/list")
 	@Transactional(readOnly = true)
 	public List<Student> list() {
 		return studentRepository.findAll();
 	}
 
-	@RequestMapping("/save")
-	@ResponseBody
-	@Transactional()
+	@RequestMapping(value = "/save")
 	public String save() {
 		Student student = new Student();
-		student.setId(4L);
 		student.setName("张三");
 		student.setAge(15L);
 		studentRepository.save(student);
 		return "保存完毕";
 	}
 
-	@RequestMapping("/update")
-	@ResponseBody
-	@Transactional()
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public String update(Long id) {
 		Student student = studentRepository.findOne(id);
-		student.setName("111");
+		student.setName("333");
 		studentRepository.saveAndFlush(student);
 		return "更新完毕";
 	}
 
-	@RequestMapping("/delete")
-	@ResponseBody
-	@Transactional()
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
 	public String delete(Long id) {
 		Student student = studentRepository.findOne(id);
 		studentRepository.delete(student);
