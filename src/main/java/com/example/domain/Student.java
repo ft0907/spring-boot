a27e4073d459ad0b65d246e7e2e193b1b3cd510e
yangfan <yangfan@yangfan-PC>
2017-03-27 22:17:26 +0800

    修改get set equals hashcode 自动生成注解

--- src/main/java/com/example/domain/Student.java
+++ src/main/java/com/example/domain/Student.java
@@ -6,14 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
-@EqualsAndHashCode
+@Data
 public class Student implements Serializable {
 
 	private static final long serialVersionUID = 659925410003167992L;
